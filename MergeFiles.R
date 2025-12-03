a6cdc177f6f1768e18d6d053ffefaae799a590d1
aarya-m <79316633+aarya-m@users.noreply.github.com>
2025-12-03 17:28:08 +0000

    MergeFiles.R

--- MergeFiles.R
+++ MergeFiles.R
@@ -6,10 +6,22 @@ head(ExtraFile)
 merged_data <- youth_not_in_education_employment_training %>%
   left_join(ExtraFile %>% select(Entity, Code, Year, pop_15_24), 
             by = c("Entity", "Code", "Year"))
+
+#Load continents CSV  
+continents <- read.csv("~/Desktop/Uni/Intro to Data Science/Group project/data sets/continents-according-to-our-world-in-data.csv") 
+head(continents)  
+
+#Add continent information  
+merged_data <- youth_not_in_education_employment_training %>%  
+  left_join(  
+    continents %>% select(Code, Continent),   # join by country Code  
+    by = "Code"  
+  )  
+
 head(merged_data)
 
 write.csv(
   merged_data,
-  file = "/Users/anna/Downloads/data sets/Youth_not_in_education_employment_training_with_continent_merged.csv", 
+  file = "/Users/anna/Downloads/data sets/Youth_neet_final.csv", 
   row.names = FALSE 
 )
